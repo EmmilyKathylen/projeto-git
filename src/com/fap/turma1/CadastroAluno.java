69ff7ddc6be180a260a95f153ff8e9f3b66647d7
Vinicius Franco <viniciusfranco00122@gmail.com>
2026-06-26 11:43:15 -0300

    modificacoes

--- src/com/fap/turma1/CadastroAluno.java
+++ src/com/fap/turma1/CadastroAluno.java
@@ -7,9 +7,36 @@ public class CadastroAluno {
 
     int idade;
 
-    void listarInformacoesAluno() {
-        System.out.println("Nome do aluno: " + nome);
-        System.out.println("E-mail do aluno: " + email);
-        System.out.println("Idade do aluno: " + idade);
+    double mensalidade;
+
+    double desconto;
+
+    void mensagemSistema() {
+        System.out.println("Seja bem-vindo ao sistema:" + nome);
+
     }
-}
+
+    void  exibirCurso(String curso) {
+        System.out.println("Curso: " + curso);
+    }
+
+    double calcularValorFinal() {
+        return mensalidade - desconto;
+    }
+
+    double calcularNotas(double nota1, double nota2, double nota3) {
+        return (nota1 + nota2 + nota3) / 3;
+    }
+
+    void verificarAprovacao(double media) {
+        if (media >= 7) {
+            System.out.println(" Aprovado");
+        }
+
+        else {
+
+            System.out.println(" Reprovado");
+        }
+
+    }
+ }
--- src/com/fap/turma1/Main.java
+++ src/com/fap/turma1/Main.java
@@ -4,14 +4,23 @@ public class Main {
     public static void main(String[] args) {
         CadastroAluno aluno1 = new CadastroAluno();
         aluno1.nome = "Emmi";
-        aluno1.email = "Emmi.com.br";
+        aluno1.email = "Emmisou@gmail.com.br";
         aluno1.idade = 23;
+        aluno1.mensalidade = 600;
+        aluno1.desconto = 150;
 
-        CadastroAluno aluno2 = new CadastroAluno();
-        aluno2.nome = "Julia";
-        aluno2.email = "Julia.com.br";
-        aluno2.idade = 18;
 
-        aluno1.listarInformacoesAluno();
+        aluno1.mensagemSistema();
+        aluno1.exibirCurso("Teste de Software");
+
+        double valorFinal = aluno1.calcularValorFinal();
+        System.out.println(valorFinal);
+
+        double media = aluno1.calcularNotas(8, 7.3, 6);
+        System.out.printf("Média: %.1f", media);
+
+        aluno1.verificarAprovacao(media);
+
+
     }
 }
